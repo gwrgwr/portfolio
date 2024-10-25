ee91fc58ab2ab796e9fad4c2959205d7cc1de613
gwrgwr <murilomassa72@gmail.com>
2024-10-25 16:28:00 -0300

    add: components on skills page

--- src/components/CardContentComponent.tsx
+++ src/components/CardContentComponent.tsx
@@ -1,14 +1,10 @@
 export const CardContentComponent = (props: {
-    text: string;
     img: string;
     alt: string;
-    level: string;
 }) => {
     return (
-        <div className="card-container rounded-lg py-4 w-56 flex flex-col gap-4 border-2 border-purple-700 shadow-dropButton items-center transform transition hover:-translate-y-3 ease-in">
-            <h1 className="text-white text-3xl">{props.text}</h1>
-            <img src={props.img} alt={props.alt} className=" object-cover bg-cover" width="100" height="100"/>
-            <p className="text-white text-xl">{props.level}</p>
+        <div className="rounded-lg p-3 w-56border-2 border-purple-700 shadow-dropButton transform transition hover:-translate-y-3 ease-in">
+            <img src={props.img} alt={props.alt} width="50" height="50"/>
         </div>
     );
 };
--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -12,31 +12,60 @@ import postmanLogo from "../assets/postman.png";
 import viteLogo from "../assets/icons8-vite-96.png";
 import gitLogo from "../assets/icons8-git-96.png";
 import javascriptLogo from "../assets/icons8-javascript-96.png";
+import linuxLogo from "../assets/icons8-linux-48.png";
+import tailwindLogo from "../assets/icons8-tailwindcss-48.png";
+import dartLogo from "../assets/icons8-dart-48.png";
+
+
 export const Skills = () => {
     return (
         <div id="skills" className="col-start-1 col-end-8 bg-gradient-to-b to-zinc-950 from-zinc-800 grid grid-cols-9 grid-rows-1 items-center justify-center">
-            <div className="self-center col-start-2 col-end-9 flex flex-col gap-6">
-                <div className="flex flex-row justify-evenly">
-                    <CardContentComponent text="React" img={reactLogo} alt="React" level="Intermediate"/>
-                    <CardContentComponent text="Java" img={javaLogo} alt="Java" level="Intermediate"/>
-                    <CardContentComponent text="MySQL" img={mysqlLogo} alt="MySQL" level="Basic"/>
-                    <CardContentComponent text="Flutter" img={flutterLogo} alt="Flutter" level="Intermediate"/>
+            <div className="self-center col-start-2 col-end-9 flex flex-row justify-between items-center">
+                <div className="flex flex-col gap-4">
+                    <h1 className="text-5xl text-white mb-4">My Skills</h1>
+                    <div className="flex flex-row gap-4">
+                        <CardContentComponent img={reactLogo} alt="React"/>
+                        <CardContentComponent img={javaLogo} alt="Java"/>
+                        <CardContentComponent img={mysqlLogo} alt="MySQL"/>
+                        <CardContentComponent img={flutterLogo} alt="Flutter"/>
+                        <CardContentComponent img={dartLogo} alt="Dart"/>
+                    </div>
+                    <div className="flex flex-row gap-4">
+                        <CardContentComponent img={typescriptLogo} alt="TypeScript"/>
+                        <CardContentComponent img={springBootLogo} alt="Spring Boot"/>
+                        <CardContentComponent img={firebaseLogo} alt="Firebase"/>
+                        <CardContentComponent img={prismaLogo} alt="Prisma"/>
+                        <CardContentComponent img={linuxLogo} alt="Linux"/>
+                    </div>
+                    <div className="flex flex-row gap-4">
+                        <CardContentComponent img={javascriptLogo} alt="JavaScript"/>
+                        <CardContentComponent img={postmanLogo} alt="Postman"/>
+                        <CardContentComponent img={viteLogo} alt="Vite"/>
+                        <CardContentComponent img={gitLogo} alt="Git"/>
+                        <CardContentComponent img={tailwindLogo} alt="TailwindCSS"/>
+                    </div>
                 </div>
-                <div className="flex flex-row justify-evenly">
-                    <CardContentComponent text="TypeScript" img={typescriptLogo} alt="TypeScript" level="Intermediate"/>
-                    <CardContentComponent text="Spring Boot" img={springBootLogo} alt="Spring Boot" level="Intermediate"/>
-                    <CardContentComponent text="Firebase" img={firebaseLogo} alt="Firebase" level="Intermediate"/>
-                    <CardContentComponent text="Prisma" img={prismaLogo} alt="Prisma" level="Intermediate"/>
-                </div>
-                <div className="flex flex-row justify-evenly">
-                    <CardContentComponent text="JavaScript" img={javascriptLogo} alt="JavaScript" level="Advanced"/>
-                    <CardContentComponent text="Postman" img={postmanLogo} alt="Postman" level="Intermediate"/>
-                    <CardContentComponent text="Vite" img={viteLogo} alt="Vite" level="Basic"/>
-                    <CardContentComponent text="Git" img={gitLogo} alt="Git" level="Intermediate"/>
+                <div className="flex flex-col gap-4 w-3/6 text-white">
+                    <p className="text-2xs text-end">
+                        I work with a versatile tech stack that allows me to develop robust applications across web,
+                        mobile, and backend. For backend development, Java is my primary choice due to its strong typing
+                        and extensive libraries, making it perfect for scalable APIs and complex business logic.
+                        Additionally, Flutter with Dart is essential in my mobile development process, enabling
+                        cross-platform apps for iOS and Android with a consistent and high-performance user experience.
+                        Dart’s efficiency in asynchronous operations complements Flutter's dynamic UI capabilities,
+                        making mobile applications responsive to real-time data.
+                    </p>
+                    <p className="text-2xs text-end">On the front end, React combined with TypeScript allows me to create interactive and robust web
+                        interfaces with added reliability from static typing, catching potential issues early in
+                        development. TailwindCSS enhances this process by providing utility classes that streamline
+                        responsive design, speeding up styling without compromising on quality. For version control, Git
+                        is essential in managing code changes and collaborating efficiently. This cohesive stack
+                        empowers me to build comprehensive applications, ensuring a seamless, well-integrated user
+                        experience across platforms.</p>
                 </div>
 
             </div>
-                <GoDownComponent href="#project"/>
+            <GoDownComponent href="#project"/>
         </div>
     );
 };
